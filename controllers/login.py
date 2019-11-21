a0f0f0e4754b0b3d4ddb9dc2c2a18e536d5d0342
Administrator <sultanofwolves@gmail.com>
2019-11-21 13:03:50 +0100

    :sparkles: Add new user functionality if not registered in db

--- controllers/login.py
+++ controllers/login.py
@@ -3,23 +3,32 @@ from bson.json_util import dumps
 from flask import request
 import firebase_admin
 from firebase_admin import auth
+from models.user import User
 
 class Login(Resource):
     def __init__(self, mongo):
         self.mongo = mongo
-        firebase_admin.initialize_app()        
+        if (not len(firebase_admin._apps)):
+            firebase_admin.initialize_app()      
 
-    def post(self): 
-        event = request.get_json(force=True)
-        print(event);
-        return dumps(event), 200
-        token = 'a'
-        decoded_token = auth.verify_id_token(token)
-        uuid = decoded_token['uid']
-        user = self.mongo.db.user.find({'UUID':uuid})
-        if user.count() == 0:
-            uuid = "new_example"
-            self.mongo.db.user.insert({'UUID':uuid})
-            user = self.mongo.db.user.find({'UUID':uuid})
-        return dumps(user), 200
-
+    def post(self):     
+        jsondata = request.get_json(force=True)
+        if jsondata is None:
+            return False
+        try:
+            decoded_token = auth.verify_id_token(jsondata['token']) 
+            current_user = auth.get_user(decoded_token['uid']) 
+            user = self.mongo.db.user.find({'UUID':current_user.uid})    
+            if user.count() == 0:
+                User.uuid = current_user.uid
+                User.name = current_user.display_name
+                User.photoURL = current_user.photo_url
+                self.mongo.db.user.insert({'name':User.name, 'UUID':User.uuid, 'photoURL':User.photoURL})
+            return True
+        except ValueError:
+            return False
+        except auth.InvalidIdTokenError:
+            return False
+        except auth.CertificateFetchError:
+            return False
+        
--- models/user.py
+++ models/user.py
@@ -2,4 +2,6 @@ from mongoengine import Document
 from mongoengine import DateTimeField, StringField, ReferenceField, ListField
 
 class User(Document):
-    UUID = StringField(max_length=60, required=True)
+    uuid = StringField(min_lenght=1, max_length=128, required=True)
+    name = StringField(max_length=100, required=True)
+    photoURL = StringField(max_length=200, required=True)
