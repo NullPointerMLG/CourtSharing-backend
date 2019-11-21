156cdc2482024a7cd270ba2614ceb514803cd9f9
JaysusM <jmartinruiz@uma.es>
2019-11-21 14:02:42 +0100

    :sparkles: Add error messages to send as answers to requests

--- resources/login.py
+++ resources/login.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from bson.json_util import dumps
+import json
 from flask import request
 import firebase_admin
 from firebase_admin import auth
@@ -11,10 +12,11 @@ class Login(Resource):
         if (not len(firebase_admin._apps)):
             firebase_admin.initialize_app()      
 
-    def post(self):     
-        jsondata = request.get_json(force=True)
-        if jsondata is None:
-            return False
+    def post(self):
+        if(not request.is_json):
+            with open('utils/errorCodes.json', 'r') as errorCodes:
+                return json.load(errorCodes)['AUTH_ERROR']['VALUE_ERROR'], 500
+        jsondata = request.get_json()
         try:
             decoded_token = auth.verify_id_token(jsondata['token']) 
             current_user = auth.get_user(decoded_token['uid']) 
@@ -26,9 +28,12 @@ class Login(Resource):
                 self.mongo.db.user.insert({'name':current_user.display_name, 'UUID':current_user.uid, 'photoURL':current_user.photo_url})
             return True
         except ValueError:
-            return False
+            with open('utils/errorCodes.json', 'r') as errorCodes:
+                return json.load(errorCodes)['AUTH_ERROR']['VALUE_ERROR'], 500
         except auth.InvalidIdTokenError:
-            return False
+            with open('utils/errorCodes.json', 'r') as errorCodes:
+                return json.load(errorCodes)['AUTH_ERROR']['INVALID_IDTOKEN_ERROR'], 500
         except auth.CertificateFetchError:
-            return False
+            with open('utils/errorCodes.json', 'r') as errorCodes:
+                return json.load(errorCodes)['AUTH_ERROR']['CERTIFICATE_FETCH_ERROR'], 500
         
