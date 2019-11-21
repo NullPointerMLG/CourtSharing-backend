82ebae56180d232e464f457aca400c5fc97eae6f
clobato <Clobato@uma.es>
2019-11-21 19:03:24 +0100

    added the way to get resource_id

--- resources/sport.py
+++ resources/sport.py
@@ -1,10 +1,19 @@
 from flask_restful import Resource, reqparse
 from bson.json_util import dumps
+<<<<<<< Updated upstream
+=======
+
+from flask import request
+>>>>>>> Stashed changes
 from bson import ObjectId
 from flask import request
 import datetime
 
+
+
 class Sport(Resource):
+    resource_id = Unique_sport()
+
     def __init__(self, mongo):
         self.mongo = mongo
 
@@ -19,5 +28,17 @@ class Sport(Resource):
 
         data = self.mongo.db.sport.aggregate(query) 
         
+<<<<<<< Updated upstream
         return dumps(data), 200
+=======
+        json = eval(dumps(data))
+
+        #Esta es una de las formas de hallar el resource_id nos podemos quedar con el 0 pq solo lo vamos a usar cuando busquemos un deporte especific
+        # por lo que solo deberia tener un elemento.
+        resource_id = json[0]['resource_id']
+        print(resource_id)
+        print(json[0]['resource_id'])
+
+        return json, 200
+>>>>>>> Stashed changes
 
