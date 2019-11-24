8091606a7297fd92aa276a065de54f81d540666f
Administrator <sultanofwolves@gmail.com>
2019-11-24 16:57:06 +0100

    :sparkles: Use of models and refactoring

--- models/sport.py
+++ models/sport.py
@@ -1,7 +1,8 @@
 from mongoengine import Document
-from mongoengine import StringField
+from mongoengine import StringField, ImageField
 
 
 class Sport(Document):
     name = StringField(max_length=60, required=True)
     resourceID = StringField(required=True)
+    icon = ImageField(required=True)
