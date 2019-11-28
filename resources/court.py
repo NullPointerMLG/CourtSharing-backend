ed48e5942a40aa52e7c02aa1ad60b9531764ba14
clobato <Clobato@uma.es>
2019-11-28 11:44:52 +0100

    change the court information directly by court

--- resources/court.py
+++ resources/court.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 from bson.json_util import dumps, loads
 from flask import request
+from bson import ObjectId
 import requests
 import datetime
 import json
@@ -13,17 +14,29 @@ class Court(Resource):
     
     def get(self):
 
-        with open('resources/resource.json', "r") as jsonFile:# Open the JSON file for reading
-            data = json.load(jsonFile) # Read the JSON into the buffer
-            jsonFile.close() # Close the JSON file
+        query = []
+        args = request.args
 
-            ## Working with buffered content
-            resource_id = data["resource"] 
+        sportID = args.get('sport-id')
+        if sportID is not None:
+            query.append({ "$match" : { "_id" : ObjectId(sportID) } })
+
+        data = self.mongo.db.sport.aggregate(query) 
+        
+        jsonAux = eval(dumps(data))
+
+        #Esta es una de las formas de hallar el resource_id nos podemos quedar con el 0 pq solo lo vamos a usar cuando busquemos un deporte especific
+        # por lo que solo deberia tener un elemento.
+        resource_id = jsonAux[0]['resource_id']
 
         response = requests.get('https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=' + resource_id) 
         data = response.json()
         data = data['result']['records']
         for record in data:
             record['INFOESP'] = loads(record['INFOESP'].replace("/", ""))
+            record.pop('PRECIOS', None)
+            record.pop('HORARIOS', None)
+            record.pop('DESCRIPCION', None)
+            record.pop('CONTACTO', None)
         return data, 200
 
--- resources/sport.py
+++ resources/sport.py
@@ -25,23 +25,6 @@ class Sport(Resource):
             query.append({ "$match" : { "_id" : ObjectId(sportID) } })
 
         data = self.mongo.db.sport.aggregate(query) 
-        
-        jsonAux = eval(dumps(data))
 
-        #Esta es una de las formas de hallar el resource_id nos podemos quedar con el 0 pq solo lo vamos a usar cuando busquemos un deporte especific
-        # por lo que solo deberia tener un elemento.
-        resource_id = jsonAux[0]['resource_id']
-        
-        with open('resources/resource.json', "r") as jsonFile:# Open the JSON file for reading
-            data = json.load(jsonFile) # Read the JSON into the buffer
-            jsonFile.close() # Close the JSON file
-            
-            data["resource"] = resource_id
-
-        ## Save our changes to JSON file
-        with open('resources/resource.json', "w+") as jsonFile:
-            jsonFile.write(json.dumps(data))
-            jsonFile.close()
-
-        return jsonAux, 200
+        return eval(dumps(data)), 200
 
