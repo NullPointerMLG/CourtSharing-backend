41ae30af9ebb3b626ab35ee1687cf312f4e0ac65
xfja <franjimenezaguilera@gmail.com>
2019-11-30 20:59:28 +0100

    :sparkles: Add comments to event get request

--- models/comment.py
+++ models/comment.py
@@ -1,9 +1,8 @@
-from mongoengine import Document
-from mongoengine import StringField, ReferenceField
+from mongoengine import Document, CASCADE, StringField, ReferenceField
 from models.user import User
 from models.event import Event
 
 class Comment(Document):
     user = ReferenceField(User, required=True)
     message = StringField(max_length=1500, required=True)
-    event = ReferenceField(Event, reverse_delete_rule=mongoengine.CASCADE)
+    event = ReferenceField(Event, reverse_delete_rule=CASCADE)
