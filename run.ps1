7c0bc628f6bbbc6dae16ddc9f93b1b02747cb0e6
Administrator <sultanofwolves@gmail.com>
2019-12-02 19:46:32 +0100

    :see_no_evil: Add powershell script to run app

---
+++ run.ps1
@@ -0,0 +1,3 @@
+venv/Scripts/activate
+$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\Administrator\Desktop\University\Ingenieria Web\Proyecto\Implementacion\CourtSharing-backend\service-account-file.json"
+python app.py
