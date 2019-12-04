41bad7dd78196a6462a935037b78f4d068b9c7f1
Administrator <sultanofwolves@gmail.com>
2019-12-04 14:06:44 +0100

    :sparkles: Return photos inside events

--- resources/events.py
+++ resources/events.py
@@ -16,9 +16,9 @@ class Events(Resource):
     def get(self):
         args = request.args
         headers = request.headers
-        #token_validation = Auth.auth_token(headers)
-       # if(token_validation != 'True'):
-          #  return token_validation
+        token_validation = Auth.auth_token(headers)
+        if(token_validation != 'True'):
+            return token_validation
 
         event_date = court_id = event_sport = None
         if args is not None:
