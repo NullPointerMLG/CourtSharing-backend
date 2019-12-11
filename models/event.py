bd77561b3fffe604d08fbb6971a5ce7a2024d810
Administrator <sultanofwolves@gmail.com>
2019-12-11 15:00:49 +0100

    :bug: Fix models and court

--- models/event.py
+++ models/event.py
@@ -1,6 +1,6 @@
 import time
 from mongoengine import Document
-from mongoengine import StringField, IntField, ReferenceField, ListField
+from mongoengine import StringField, IntField, ReferenceField, ListField, CASCADE
 from models.user import User
 from models.sport import Sport
 
@@ -11,6 +11,6 @@ class Event(Document):
     description = StringField(max_length=1500)
     court_id = IntField(required = True)
     creator = ReferenceField(User, required=True)
-    sport = ReferenceField(Sport, required=True)
+    sport = ReferenceField(Sport, required=True, reverse_delete_rule=CASCADE)
     photos = ListField(StringField(required=False))
     participants = ListField(ReferenceField(User), required = True)
