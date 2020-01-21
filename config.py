e174cda0ed29fb7372cfa095922cc7bbd4a0d83a
Administrator <sultanofwolves@gmail.com>
2020-01-21 20:22:57 +0100

    make it better

---
+++ config.py
@@ -0,0 +1,12 @@
+MONGO_URL = "mongodb+srv://ingewebuma:ingeweb2019@iweb-yezxy.mongodb.net/iweb"
+
+FIREBASE_CONFIG = {
+                "apiKey": "AIzaSyBH4q0c3Uygge--a8zNYS432wd_1DOxMD4",
+                "authDomain": "courtsharing.firebaseapp.com",
+                "databaseURL": "https://courtsharing.firebaseio.com",
+                "projectId": "courtsharing",
+                "storageBucket": "courtsharing.appspot.com",
+                "messagingSenderId": "596174788055",
+                "appId": "1:596174788055:web:072710db71f12870a2a792",
+                "measurementId": "G-JX3QQ63MEL"
+                 }
--- run.ps1
+++ run.ps1
@@ -1,3 +1,3 @@
 venv/Scripts/activate
-$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\Administrator\Desktop\University\Ingenieria Web\Proyecto\Implementacion\CourtSharing-backend\service-account-file.json"
+$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\Administrator\Desktop\University\Ingenieria Web\Proyecto\Tercera entrega - Platform as a Service\CourtSharing-backend\service-account-file.json"
 python app.py
